31ccf8b31335ffb0c6745e95104bbeb2ed4ea726
Prabhjot Singh <psingh20112002@gmail.com>
2021-08-01 10:45:02 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Accordian from './Accordian/Accordian';
+import   './Accordian/accordian.css';
+function App() {
+  return (
+    <Accordian/>
+  );
+}
+
+export default App;
